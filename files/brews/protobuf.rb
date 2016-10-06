d69d2dfb04d688f1f866f1dafaa0dc2363467b68
Matt Corley <mcorley@signalfx.com>
2016-10-06 11:03:03 -0700

    Update protobuf.rb
    Fix protobuf URL

--- files/brews/protobuf.rb
+++ files/brews/protobuf.rb
@@ -5,7 +5,7 @@ require 'formula'
 
 class Protobuf < Formula
   homepage 'http://code.google.com/p/protobuf/'
-  url 'http://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2'
+  url 'https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.bz2'
   sha1 '62c10dcdac4b69cc8c6bb19f73db40c264cb2726'
 
   version '2.5.0-boxen1'
